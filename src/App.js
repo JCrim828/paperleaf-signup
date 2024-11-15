ca65d1027ca650adcb364edb5ffea131f5da256f
Jack Criminger <jack.crim828@gmail.com>
2024-11-15 14:47:40 -0800

    test commit 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { useState } from 'react';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function Message(props) {
+  return <h1>{props.text}</h1>;
 }
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Banner({text}) {
+  return (
+  <header className="banner">{text}</header>
+  );
+}
+
+export default function webPage(){
+  
+  function Square({ character }) {
+    return (
+    <button className="square">{character}</button>
+    );
+  }
+
+  return (
+<div>
+  <div>
+    <Banner text= "*Logo*" />
+    <Square character="J"/>
+    <Square character="A"/>
+    <Square character="C"/>
+    <Square character="K"/>
+  </div>
+  <Message text="Jack Criminger" />
+</div>
+  );
 }
